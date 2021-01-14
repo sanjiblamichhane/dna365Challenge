9f65296dc9b5a763a182e45a0c4ca2a3697a7593
sanjiblamichhane <slamich000@citymail.cuny.edu>
2021-01-14 18:46:26 -0500

    travelling salesman alg initial commit

--- coursera_DataStructure_Cpp/week2/Week2_Challenge/main_w2.cc
+++ coursera_DataStructure_Cpp/week2/Week2_Challenge/main_w2.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "w2.h"
 using namespace std;
 
 int main()
---
+++ coursera_DataStructure_Cpp/week2/Week2_Challenge/skel_w2.h
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// This class Pair has already been defined for you.
+// (You may not change this definition.)
+class Pair
+{
+	//class
+	public:
+		int first,second;
+
+		// method
+		void check()
+		{
+			first = 5;
+			cout<<" Congratulations! The check() method of the Pair class \n
+				has execuded. But this isn't enough to guarantee \n
+				that your code is correct" <<endl;
+		}
+}
+
+// Insert your declaration and implementation of the function pairFactory()
+// below, by replacing "..." with proper C++ code. Be sure to declare the
+// function type to return a pointer to a Pair.
+
+... pairFactory() {
+  // ...
+  // (You can use as many lines as you want.)
+  return ...;
+}
+
+// Your function should be able to satisfy the tests below. You should try
+// some other things to convince yourself. If you have a bug in this problem,
+// the usual symptom is that after you submit, the grader will crash with a
+// system error. :-)
+int main() {
+  Pair *p;
+  p = pairFactory();
+  
+  // This function call should work without crashing:
+  p->check();
+  
+  // Deallocating the heap memory. (Assuming it was made on the heap!)
+  delete p;
+
+  std::cout << "If you can see this text, the system hasn't crashed yet!" << std::endl;  
+
+  return 0;
+}
